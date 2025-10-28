34de6a14c961c27d5d4b46d5d28e7267bd1d13a9
vaghanihet2001 <vaghanihet2001@gmail.com>
2025-10-28 15:13:22 +0530

    adding custom domain to rander webservice

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: process.env.PORT || 5000,
-    allowedHosts: ["hexflow.onrender.com"], // 👈 add your Render domain here
+    allowedHosts: ["www.hexverce.in","hexflow.onrender.com"], // 👈 add your Render domain here
   },
 });
